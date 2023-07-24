db0f298fa5f16f9c960f2e05d50b174fb1f24bfa
Evgeny <eebelyaev@gmail.com>
2023-07-24 12:02:37 +0300

    ch05/task13

--- chapter05/task13/main.go
+++ chapter05/task13/main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"golearn/chapter05/iohtml"
-	"io/ioutil"
+	"golearn/chapter05/links"
+	"io"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
-	"golang.org/x/net/html"
 )
 
 const SAVED_DOMAIN = "https://go.dev/"
@@ -21,64 +20,51 @@ func main() {
 
 func crawl(url string) []string {
 	fmt.Println(url)
-	list, err := extract(url)
+	list, err := links.Extract(url)
 	if err != nil {
 		log.Print(err)
+	} else {
+		for _, u := range list {
+			if strings.HasPrefix(u, SAVED_DOMAIN) {
+				savePage(u)
+			}
+		}
 	}
 	return list
 }
 
-func savePage(resp *http.Response, url string) error {
-	if strings.HasPrefix(url, SAVED_DOMAIN) {
-		path, _ := strings.CutPrefix(url, SAVED_DOMAIN)
-		path = "res/" + strings.Trim(path, "/")
-		err := os.MkdirAll(path, 0750)
+// savePage сохраняет указанную страницу в соответствующем каталоге.
+// Если каталога не существует, он создается.
+func savePage(url string) error {
+	path, _ := strings.CutPrefix(url, SAVED_DOMAIN)
+	if path = strings.Trim(path, "/"); len(path) > 0 {
+		path = "res/" + path
+	} else {
+		path = "res"
+	}
+	// проверим отсутствие файла
+	if _, err := os.Stat(path + "/index.html"); errors.Is(err, os.ErrNotExist) {
+		err = os.MkdirAll(path, 0750)
 		if err != nil && !os.IsExist(err) {
 			return err
 		}
-		body, err := ioutil.ReadAll(resp.Body)
+
+		resp, err := http.Get(url)
 		if err != nil {
 			return err
 		}
-		return ioutil.WriteFile(path+"/index.html", body, 0644)
+		defer resp.Body.Close()
+		if resp.StatusCode != http.StatusOK {
+			return fmt.Errorf("получение %s: %s", url, resp.Status)
+		}
+
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			return err
+		}
+		return os.WriteFile(path+"/index.html", body, 0644)
 	}
 	return nil
-}
-
-// extract выполняет HTTP-запрос GET по определенному URL, выполняет
-// синтаксический анализ HTML и возвращает ссылки в HTML-документе.
-func extract(url string) ([]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode != http.StatusOK {
-		resp.Body.Close()
-		return nil, fmt.Errorf("получение %s: %s", url, resp.Status)
-	}
-	savePage(resp, url)
-	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		return nil, fmt.Errorf("анализ %s как HTML: %v", url, err)
-	}
-	var links []string
-	visitNode := func(n *html.Node) {
-		if n.Type == html.ElementNode && n.Data == "a" {
-			for _, a := range n.Attr {
-				if a.Key != "href" {
-					continue
-				}
-				link, err := resp.Request.URL.Parse(a.Val)
-				if err != nil {
-					continue // Игнорируем некорректные URL
-				}
-				links = append(links, link.String())
-			}
-		}
-	}
-	iohtml.ForEachNode(doc, visitNode, nil)
-	return links, nil
 }
 
 // breadthFirst вызывает f для каждого элемента в worklist.
