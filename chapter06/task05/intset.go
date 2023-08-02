144395751ea45ceb1fe969eea344b2f86c8df882
Evgeny <eebelyaev@gmail.com>
2023-08-02 18:46:11 +0300

    ch06/task05

---
+++ chapter06/task05/intset.go
@@ -0,0 +1,179 @@
+package intset
+
+import (
+	"bytes"
+	"fmt"
+)
+
+// IntSet представляет собой множество небольших неотрицательных
+// целых чисел. Нулевое значение представляет пустое множество.
+type IntSet struct {
+	words []uint
+}
+
+// BIT_COUNT - количество разрядов.
+const BIT_COUNT = 32 << (^uint(0) >> 63)
+
+// Has указывает, содержит ли множество неотрицательное значение х.
+func (s *IntSet) Has(x int) bool {
+	word, bit := x/BIT_COUNT, uint(x%BIT_COUNT)
+	return word < len(s.words) && s.words[word]&(1<<bit) != 0
+}
+
+// Add добавляет неотрицательное значение x в множество,
+func (s *IntSet) Add(x int) {
+	word, bit := x/BIT_COUNT, uint(x%BIT_COUNT)
+	for word >= len(s.words) {
+		s.words = append(s.words, 0)
+	}
+	s.words[word] |= 1 << bit
+}
+
+// UnionWith делает множество s равным объединению множеств s и t.
+func (s *IntSet) UnionWith(t *IntSet) {
+	for i, tword := range t.words {
+		if i < len(s.words) {
+			s.words[i] |= tword
+		} else {
+			s.words = append(s.words, tword)
+		}
+	}
+}
+
+// String возвращает множество как строку вида "{1 2 3}".
+func (s *IntSet) String() string {
+	var buf bytes.Buffer
+	buf.WriteByte('{')
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < BIT_COUNT; j++ {
+			if word&(1<<uint(j)) != 0 {
+				if buf.Len() > len("{") {
+					buf.WriteByte(' ')
+				}
+				fmt.Fprintf(&buf, "%d", BIT_COUNT*i+j)
+			}
+		}
+	}
+	buf.WriteByte('}')
+	return buf.String()
+}
+
+// Len Возвращает количество элементов.
+func (s *IntSet) Len() (res int) {
+	for _, w := range s.words {
+		for i := 0; i < BIT_COUNT; i++ {
+			res += int(w & (1 << i) >> i)
+		}
+	}
+	return res
+}
+
+// Remove Удаляет x из множества.
+func (s *IntSet) Remove(x int) {
+	word, bit := x/BIT_COUNT, uint(x%BIT_COUNT)
+	if word >= len(s.words) {
+		return
+	}
+	s.words[word] &^= 1 << bit
+}
+
+// Clear Удаляет все элементы множества.
+func (s *IntSet) Clear() {
+	s.words = nil
+}
+
+// Copy Возвращает копию множества.
+func (s *IntSet) Copy() *IntSet {
+	var cp IntSet
+	if s.words != nil {
+		cp.words = append(make([]uint, 0), s.words...)
+	}
+	return &cp
+}
+
+// Compare сравнивает два множества.
+func Compare(s1, s2 IntSet) error {
+	if s1.Len() != s2.Len() {
+		return fmt.Errorf("отличается длина: s1.Len = %d, s2.Len = %d",
+			s1.Len(), s2.Len())
+	}
+	for i, w := range s1.words {
+		if s2.words[i] != w {
+			return fmt.Errorf("элементы слайсов words[%d] отличаются: %d != %d",
+				i, w, s2.words[i])
+		}
+	}
+	return nil
+}
+
+// AddAll позволяет добавлять список значений.
+func (s *IntSet) AddAll(vals ...int) {
+	for _, v := range vals {
+		s.Add(v)
+	}
+}
+
+// IntersectWith - пересечение множеств.
+func (s *IntSet) IntersectWith(s2 *IntSet) {
+	if len(s.words) > len(s2.words) {
+		s.words = s.words[:len(s2.words)]
+	}
+	for i := len(s.words) - 1; i >= 0; i-- {
+		s.words[i] &= s2.words[i]
+		if s.words[i] == 0 && len(s.words)-1 == i {
+			s.words = s.words[:i]
+		}
+	}
+}
+
+// DifferenceWith вычитает переданное множество из множества-получателя.
+func (s *IntSet) DifferenceWith(s2 *IntSet) {
+	for i := len(s.words) - 1; i >= 0; i-- {
+		if i < len(s2.words) {
+			s.words[i] &^= s2.words[i]
+			if s.words[i] == 0 && len(s.words)-1 == i {
+				s.words = s.words[:i]
+			}
+		}
+	}
+}
+
+// SymmetricDifference - Симметричная разность двух множеств содержит
+// элементы, имеющиеся в одном из множеств, но не в обоих одновременно.
+func (s *IntSet) SymmetricDifference(s2 *IntSet) {
+	cut := true
+	minlen := len(s2.words)
+	if minlen > len(s.words) {
+		minlen = len(s.words)
+		s.words = append(s.words, s2.words[minlen:]...)
+		cut = false
+	}
+	for i := minlen - 1; i >= 0; i-- {
+		s.words[i] ^= s2.words[i]
+		if cut && s.words[i] == 0 && len(s.words)-1 == i {
+			s.words = s.words[:i]
+		}
+	}
+	// cs := s.Copy()
+	// s.DifferenceWith(s2)
+	// cs2 := s2.Copy()
+	// cs2.DifferenceWith(cs)
+	// s.UnionWith(cs2)
+}
+
+func (s *IntSet) Elems() (elems []int) {
+	for i, word := range s.words {
+		if word == 0 {
+			continue
+		}
+		for j := 0; j < BIT_COUNT; j++ {
+			if word&(1<<uint(j)) != 0 {
+				elems = append(elems, BIT_COUNT*i+j)
+			}
+		}
+	}
+	return
+}
